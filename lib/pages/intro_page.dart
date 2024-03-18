0013c04e64b222840ba06b0b7c6cfd82f1f96ab9
prabhsuratsingh <prabhsurat17@gmail.com>
2024-03-18 20:04:10 +0530

    First Commit
    Created intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Image.asset(
+                  'lib/images/logo_transparent.png',
+                  height: 240,
+                ),
+              ),
+          
+              const SizedBox(height: 30,),
+          
+              //title
+              const Text(
+                'Get Funky,\nIndian Style!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30
+                ),
+              ),
+          
+              const SizedBox(height: 30,),
+          
+              //subtitle
+              const Text(
+                'Pure Indian Flex😉',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+                textAlign: TextAlign.center,
+              ),
+          
+              const SizedBox(height: 80,),
+
+              //start button
+              GestureDetector(
+                // onTap: () => Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => HomePage(),
+                //   )
+                // ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
