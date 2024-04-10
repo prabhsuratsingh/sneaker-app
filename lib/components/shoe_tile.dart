fa535d8b61d0dfd0f9f50c6a5bf0b307ebc6c9a4
prabhsuratsingh <prabhsurat17@gmail.com>
2024-04-10 13:57:43 +0530

    Created shoe tile

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,77 @@
+import 'package:ecommerce_ui/models/shoe.dart';
+import 'package:flutter/material.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //shoe picture
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+            ),
+            child: Image.asset(shoe.imagePath),
+          ),
+          
+          //shoe description
+          Text(shoe.description),
+
+          //shoe price
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //shoe name
+                    Text(
+                      shoe.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20
+                      ),
+                    ),
+            
+                    const SizedBox(height: 5,),
+            
+                    //price
+                    Text('\$'+shoe.price),
+                  ],
+                ),
+                //plus button
+                Container(
+                  padding:const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12)
+                    ),
+                  ),
+                  child: const Icon(Icons.add)
+                ),
+              ],
+            ),
+          ),
+          //add to cart button
+        ],
+      ),
+    );
+  }
+} 
